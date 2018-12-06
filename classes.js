e63f9c3abaec1a1fa00db96e80ff8e6f6f0f73e8
Juan Soto <38800554+justjunior89@users.noreply.github.com>
2018-12-05 22:12:29 -0800

    Classes
    practicing vanilla javascript with classes

---
+++ classes.js
@@ -0,0 +1,52 @@
+/*
+let halley = {
+  _name: 'Halley',
+  _behavior: 0,
+
+  get name() {
+    return this._name;
+  },
+
+  get behavior() {
+    return this._behavior;
+  },
+
+  incrementBehavior() {
+    this._behavior++;
+  }
+}
+
+*/
+class Dog {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+  get behavior() {
+    return this._behavior;
+  }   
+
+  incrementBehavior() {
+    this._behavior ++;
+  }
+}
+
+const halley = new Dog('Halley');
+console.log(halley.name); // Print name value to console
+console.log(halley.behavior); // Print behavior value to console
+halley.incrementBehavior(); // Add one to behavior
+console.log(halley.name); // Print name value to console
+console.log(halley.behavior); // Print behavior value to console
+// ****************************************************************************************************************************************
+
+// initialize object construction
+class Surgeon {
+  constructor(name,department) {
+    this.name = name;
+    this.department = department;
+  }
+}
