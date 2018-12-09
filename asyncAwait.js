fda9beea9c5e941eecf5d1494b4f67e6e02dce8d
Juan Soto <38800554+justjunior89@users.noreply.github.com>
2018-12-09 00:18:40 -0800

    Create asyncAwait.js
    Initial commit

---
+++ asyncAwait.js
@@ -0,0 +1,180 @@
+// ----------------------------------------------------------------------------------------------------------------------------------
+
+const fs = require('fs');
+const promisifiedReadfile = require('./promisifiedReadfile');
+      
+// Here we use fs.readfile() and callback functions:
+fs.readFile('./file.txt', 'utf-8', (err, data) => {
+  if (err) throw err;
+  let firstSentence = data;
+  fs.readFile('./file2.txt',  'utf-8', (err, data) => {
+    if (err) throw err;
+    let secondSentence = data;
+    console.log(firstSentence, secondSentence)
+  });
+});
+
+// Here we use native promises with our "promisified" version of readfile:
+let firstSentence
+promisifiedReadfile('./file.txt', 'utf-8')
+  .then((data) => {
+    firstSentence = data;
+    return promisifiedReadfile('./file2.txt', 'utf-8')
+  })
+  .then((data) => {
+    let secondSentence = data;
+    console.log(firstSentence, secondSentence)
+  })
+  .catch((err) => {console.log(err)});
+
+// Here we use promisifiedReadfile() again but instead of using the native promise .then() syntax, we declare and invoke an async/await function:
+async function readFiles() {
+  let firstSentence = await promisifiedReadfile('./file.txt', 'utf-8')
+  let secondSentence = await promisifiedReadfile('./file2.txt', 'utf-8')
+  console.log(firstSentence, secondSentence)
+}
+readFiles()
+
+
+
+// The async Keyword ------------------------------------------------------------------------------------------------------------------
+
+
+
+function withConstructor(num){
+  return new Promise((resolve, reject) => {
+    if (num === 0){
+      resolve('zero');
+    } else {
+      resolve('not zero');
+    }
+  })
+}
+
+withConstructor(0)
+  .then((resolveValue) => {
+  console.log(` withConstructor(0) returned a promise which resolved to: ${resolveValue}.`);
+})
+
+// Write your code below:
+async function withAsync(num){
+  if (num === 0){
+      return 'zero';
+    } else {
+      return 'not zero';
+    }
+}
+
+withAsync(100)
+  .then((resolveValue) => {
+  console.log(` withAsync(100) returned a promise which resolved to: ${resolveValue}.`);
+})
+
+
+
+// The await operator ------------------------------------------------------------------------------------------------------------------
+
+
+
+const brainstormDinner = require('./library.js')
+
+
+// Native promise version:
+function nativePromiseDinner() {
+  brainstormDinner().then((meal) => {
+	  console.log(`I'm going to make ${meal} for dinner.`);
+  })
+}
+
+
+// async/await version:
+async function announceDinner() {
+  // Write your code below:
+  let meal = await brainstormDinner();
+  console.log(`I'm going to make ${meal} for dinner.`);
+}
+
+announceDinner()
+
+// Writing async functions -------------------------------------------------------------------------------------------------------------
+
+
+const shopForBeans = require('./library.js');
+
+async function getBeans() {
+  console.log(`1. Heading to the store to buy beans...`);
+  let value = await shopForBeans();
+  console.log(`3. Great! I'm making ${value} beans for dinner tonight!`);
+}
+
+getBeans();
+
+
+// Handling Dependant Promises --------------------------------------------------------------------------------------------------
+
+
+const {shopForBeans, soakTheBeans, cookTheBeans} = require('./library.js');
+
+// Write your code below:
+async function makeBeans() {
+  let type = await shopForBeans();
+  let isSoft = await soakTheBeans(type);
+  let dinner = await cookTheBeans(isSoft);
+  console.log(dinner);
+}
+
+makeBeans();
+
+
+// Handling Errors -------------------------------------------------------------------------------------------------------------
+
+const cookBeanSouffle = require('./library.js');
+
+// Write your code below:
+
+async function hostDinnerParty() {
+ try {
+   let dinner = await cookBeanSouffle();
+   console.log(`${dinner} is served!`);
+ }
+ catch(error){
+   console.log(error);
+   console.log('Ordering a pizza!');
+ }
+}
+
+hostDinnerParty();
+
+
+
+// Handling Independant Promises ----------------------------------------------------------------------------------------
+
+
+let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./library.js')
+
+// Write your code below:
+
+async function serveDinner() {
+ let vegetablePromise = steamBroccoli();
+ let starchPromise = cookRice();
+ let proteinPromise = bakeChicken();
+ let sidePromise = cookBeans();
+ console.log(`Dinner is served. We're having ${await vegetablePromise}, ${await starchPromise}, ${await proteinPromise}, and ${await sidePromise}.`)
+}
+
+serveDinner()
+
+
+// Await Promise.all() ----------------------------------------------------------------------------------------
+
+
+let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./library.js')
+
+// Write your code below:
+async function serveDinnerAgain(){
+  let foodArray = await Promise.all([steamBroccoli(), cookRice(), bakeChicken(), cookBeans()]); 
+  
+  console.log(`Dinner is served. We're having ${foodArray[0]}, ${foodArray[1]}, ${foodArray[2]}, and ${foodArray[3]}.`)
+}
+
+serveDinnerAgain()
