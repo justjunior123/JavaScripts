7a093aa134c73d81c8935d7ee4b1d09b0bce0bb6
Juan Soto <38800554+justjunior89@users.noreply.github.com>
2018-12-09 00:53:42 -0800

    Update functions.js
    Update

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-// Functions examples ------------------------------------------------------------------------------------------------------------------
+// Function Declarations ------------------------------------------------------------------------------------------------------------------
 
 function getReminder(){
   console.log("Water the plants")
@@ -7,7 +7,7 @@ function getReminder(){
 function greetInSpanish(){
 	console.log('Buenas Tardes.')  
 }
-
+// Calling a Function------------------------------------------------------------------------------------------------------------------
 function sayThanks(){
   console.log('Thank you for your purchase! We appreciate your business.')
 }
@@ -15,19 +15,25 @@ function sayThanks(){
 sayThanks();
 sayThanks();
 sayThanks();
-//------------------------------------------------------------------------------------------------------------------
+
+
+//Parameters and Arguements ------------------------------------------------------------------------------------------------------------
 function sayThanks(name) {
   console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
 }
 
 sayThanks("Cole")
-//------------------------------------------------------------------------------------------------------------------
+
+// Default Parameters ------------------------------------------------------------------------------------------------------------------
+
 function makeShoppingList(item1 = 'milk', item2='bread', item3='eggs'){
   console.log(`Remember to buy ${item1}`);
   console.log(`Remember to buy ${item2}`);
   console.log(`Remember to buy ${item3}`);
 }
 
+
+//Return ------------------------------------------------------------------------------------------------------------------
 function monitorCount(rows,columns) {
   return rows * columns
 }
@@ -36,7 +42,7 @@ const numOfMonitors = monitorCount(5,4)
 
 console.log(`${numOfMonitors}`)
 
-//------------------------------------------------------------------------------------------------------------------
+// Helper Functions ------------------------------------------------------------------------------------------------------------------
 
 function monitorCount(rows, columns) {
   return rows * columns;
@@ -51,6 +57,7 @@ const totalCost = costOfMonitors(5,4)
 
 console.log(`${totalCost}`)
 
+// Function Expressions ------------------------------------------------------------------------------------------------------------------
 
 const plantNeedsWater = function(day){
 	if(day === 'Wednesday'){
@@ -61,8 +68,23 @@ const plantNeedsWater = function(day){
 
 console.log(plantNeedsWater('Tuesday'))
 
+// Arrow Functions------------------------------------------------------------------------------------------------------------------
+
+/*const plantNeedsWater = function(day) {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};*/
+
+const plantNeedsWater = (day) =>
+	{
+    
+}
 
 
-//------------------------------------------------------------------------------------------------------------------
+// Concise Body Arrow Functions  ----------------------------------------------------------------------------------------------------
 
 const plantNeedsWater = day => day === 'Wednesday' ? true : false;
+
