ac81cae773c3b17dac0340abc853cdf0a505046e
Juan Soto <38800554+justjunior89@users.noreply.github.com>
2018-12-08 23:37:30 -0800

    Create objects.js
    Initial commit

---
+++ objects.js
@@ -0,0 +1,159 @@
+// Objects Excercises  ---------------------------------------------------------------------------------------------------------------
+
+const fasterShip = {
+  'Fuel Type': 'Turbo Fuel',
+  color: 'silver'
+};
+
+// ---------------------------------------------------------------------------------------------------------------
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+const crewCount = spaceship.numCrew
+const planetArray = spaceship.flightPath
+
+// ---------------------------------------------------------------------------------------------------------------
+
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  'Active Mission' : true,
+  homePlanet : 'Earth', 
+  numCrew: 5
+ };
+
+let propName =  'Active Mission';
+
+// Write your code below
+const isActive = spaceship["Active Mission"]
+console.log(spaceship[propName])
+
+// ---------------------------------------------------------------------------------------------------------------
+
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+spaceship.color = 'glorious gold'
+
+spaceship.numEngines = 4
+
+delete spaceship['Secret Mission']
+
+// ---------------------------------------------------------------------------------------------------------------
+
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+// Write your code below
+
+let alienShip = {
+  retreat() {
+    console.log(retreatMessage)
+  },
+  takeOff() {
+    console.log('Spim... Borp... Glix... Blastoff!')
+  }
+};
+
+alienShip.retreat();
+
+alienShip.takeOff();
+
+// ---------------------------------------------------------------------------------------------------------------
+
+let spaceship = {
+  passengers: null,
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032 
+  },
+  crew: {
+    captain: { 
+      name: 'Sandra', 
+      degree: 'Computer Engineering', 
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    backup: {
+      battery: "Lithium",
+      terabytes: 50
+    }
+  }
+};
+const capFave = spaceship.crew.captain['favorite foods'][0]
+
+spaceship.passengers = [{name:'her'}]
+console.log(spaceship)
+const firstPassenger = { name: 'Juan'}
+console.log(firstPassenger)
+spaceship.passengers[0] = firstPassenger
+
+// ---------------------------------------------------------------------------------------------------------------
+
+
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
+};
+
+// Write your code below
+const greenEnergy = obj => {
+  obj['Fuel Type'] = 'avocado oil'
+}
+
+const remotelyDisable = obj => {
+  obj.disabled = true
+}
+
+greenEnergy(spaceship)
+remotelyDisable(spaceship)
+console.log(spaceship)
+
+
+// ---------------------------------------------------------------------------------------------------------------
+
+let spaceship = {
+    crew: {
+    captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+        },
+    'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+        },
+    medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() { console.log(`Jets on!`) } },
+    translator: {
+        name: 'Shauna', 
+        degree: 'Conservation Science', 
+        powerFuel() { console.log('The tank is full!') } 
+        }
+    }
+}; 
+
+// Write your code below
+
+for (let crewMember in spaceship.crew){
+  console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`)
+}
